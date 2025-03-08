6d09d7b4d29c0ccdf61366883cf823cda1577ecf
Thunder Vikes <thundervikes10166@gmail.com>
2025-03-07 21:49:14 -0800

    Project code going into day 1 of VC Regional 25

--- 1st Robot 10166/src/main/java/frc/robot/Robot.java
+++ 1st Robot 10166/src/main/java/frc/robot/Robot.java
@@ -20,6 +20,8 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 
+import com.revrobotics.spark.SparkFlex;
+
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
@@ -39,22 +41,41 @@ private Command m_autonomousCommand;
 
 private DifferentialDrive m_myRobot;
 
+private final Timer timer = new Timer();
 
  // int leftMotor = 3; // this is the motor CAN ID 
   int diff_drive_motor_canID_right = 2; //this is the motor CAN ID 
-  int diff_drive_motor_canID_left = 3;
+  int diff_drive_motor_canID_right_follow = 3; //this is the motor CAN ID 
+  
+  int diff_drive_motor_canID_left = 8;
+  int diff_drive_motor_canID_left_follow = 6;
+
   int elevator_motor_canID_left = 4;
+  int elevator_motor_canID_right = 9;
+
+  int claw_motor_canID_left = 7;
+  int claw_motor_canID_right = 10;
+
 
   public SparkMax chassis_motor_left = new SparkMax(diff_drive_motor_canID_left, SparkLowLevel.MotorType.kBrushed); // 0 is the RIO PWM port this is connected to
+  public SparkMax chassis_motor_left_follow = new SparkMax(diff_drive_motor_canID_left_follow, SparkLowLevel.MotorType.kBrushed);
+  
   public SparkMax chassis_motor_right = new SparkMax(diff_drive_motor_canID_right, SparkLowLevel.MotorType.kBrushed);
-  public SparkMax elevat_motor_left = new SparkMax(elevator_motor_canID_left, SparkLowLevel.MotorType.kBrushless);  
+  public SparkMax chassis_motor_right_follow = new SparkMax(diff_drive_motor_canID_right_follow, SparkLowLevel.MotorType.kBrushed);
+  
+  public SparkFlex claw_motor_left = new SparkFlex(claw_motor_canID_left, SparkLowLevel.MotorType.kBrushed);
+  public SparkFlex claw_motor_right = new SparkFlex(claw_motor_canID_right, SparkLowLevel.MotorType.kBrushed);
+
+  
+  public SparkMax elevat_motor_left = new SparkMax(elevator_motor_canID_left, SparkLowLevel.MotorType.kBrushless); 
+  public SparkMax elevat_motor_right = new SparkMax(elevator_motor_canID_right, SparkLowLevel.MotorType.kBrushless);  
   public RelativeEncoder elevatorEncoder = elevat_motor_left.getEncoder();
+  public RelativeEncoder elevatorREncoder = elevat_motor_right.getEncoder();
 
 
-  private XboxController controller;
-  private Joystick chassis_stick_right;
-
-  public boolean B_button; 
+ 
+  
+  private XboxController controller1 = new XboxController(0);
 
 
   private final RobotContainer m_robotContainer;
@@ -70,14 +91,50 @@ private DifferentialDrive m_myRobot;
         // result in both sides moving forward. Depending on how your robot's
   
         
-        m_myRobot = new DifferentialDrive(chassis_motor_left, chassis_motor_right);
+        m_myRobot = new DifferentialDrive(chassis_motor_left, chassis_motor_right); 
+        m_myRobot = new DifferentialDrive(chassis_motor_left_follow, chassis_motor_right_follow); // we add this one
+
+
         //       chassis_motor_left.configure(SparkBaseConfig, SparkBase.ResetMode, SparkBase.PersistMode);
+       
+       // chassis_motor_left_follow.set(chassis_motor_left.get());
+       // chassis_motor_right_follow.set(chassis_motor_right.get());
         
-        controller = new XboxController(0);
+
+       // controller1 = new XboxController(0);
+      
+
         //chassis_stick_right = new Joystick(1);
        
-        chassis_motor_right.setInverted(true);
+    
+
+        
+  
+        chassis_motor_right.setInverted(true);// we change chassis_motor_right to left to make the chassis move forward
+        chassis_motor_right_follow.setInverted(true);
+
+
         m_robotContainer = new RobotContainer();
+      }
+
+      public void runMotorAuto(double xSpeed,double RotaionSpeed){
+        m_myRobot.arcadeDrive(xSpeed, RotaionSpeed);
+
+      }
+
+      public void runTeleop(XboxController controller){
+
+        double Xinput = controller.getLeftY();
+        double Rotate = controller.getLeftX();
+
+        m_myRobot.arcadeDrive(Xinput, Rotate);
+      }
+
+      public double getEncoderDrive(){
+       double encoderPosition =  chassis_motor_left.getEncoder().getPosition();
+
+       return encoderPosition;
+
       }
    
     
@@ -114,7 +171,18 @@ private DifferentialDrive m_myRobot;
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    //double time = Timer.getFPGATimestamp();//add
+
+    if(timer.advanceIfElapsed(0.1)){
+      runMotorAuto(.25, 0);
+    }else if (timer.advanceIfElapsed(3)){
+      runMotorAuto(0, 0);
+    }
+
+  
+  
+  }
 
   @Override
   public void teleopInit() {
@@ -135,17 +203,98 @@ private DifferentialDrive m_myRobot;
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-   m_myRobot.arcadeDrive(controller.getLeftY(), controller.getLeftX());
+
+    runTeleop(controller1);
+
+
+
+    double initialencoder;
+    int Level1;
+    int Level2;
+    int Level3;
+    double midcoder;
+    double topcoder;
+    double initialenencoder;
+
+  //m_myRobot.arcadeDrive(controller1.getLeftY(), controller1.getLeftX());
+ 
    SmartDashboard.putNumber("encoder position",elevatorEncoder.getPosition());
    SmartDashboard.putNumber("encoder velocity",elevatorEncoder.getVelocity());
 
-   if(controller.getAButtonPressed()){
-    elevat_motor_left.set(0.5);
+
+   //when we press left stick button robot turn left
+   /**  if(controller.getLeftStickButton()){
+     chassis_motor_left.set(-0.5);
+     }else if(controller.getLeftStickButton()){
+     chassis_motor_left.set(0);
+    }
+
+    if(controller.getRightStickButton()){
+      chassis_motor_right.set(0.5);
+      }else if(controller.getRightStickButton()){
+      chassis_motor_right.set(0);
+      }*/
+
+ 
+   /* if(controller.getAButtonPressed()){
+    initialencoder = elevatorEncoder.getPosition();
+    while((elevatorEncoder.getPosition() - initialencoder) <= 0.1){
+    elevat_motor_left.set(-0.5);
+    elevat_motor_right.set(-0.5);
+    }
    }else if(controller.getAButtonReleased()){
+    midcoder = elevatorEncoder.getPosition();
+    while ((elevatorEncoder.getPosition() - midcoder ) <= 0.1) {
+    elevat_motor_left.set(0.5);
+    elevat_motor_right.set(0.5);
+    }
+  
+   } */
+
+   /*if(controller.getAButtonPressed()){
+    initialencoder = elevatorEncoder.getPosition();
+    elevat_motor_left.set(0.5);
+    //elevat_motor_right.set(0.5);
+   }
+   else{
     elevat_motor_left.set(0);
    }
+   */
+  claw_motor_left.setInverted(true);
+   
+   if(controller1.getBButtonPressed()){
+    elevat_motor_left.set(5);
+    elevat_motor_right.set(5);
+  }else if(controller1.getBButtonReleased()){
+    elevat_motor_left.set(0);
+    elevat_motor_right.set(0);
+   }
+  
+   if(controller1.getAButtonPressed()){
+    elevat_motor_left.set(-5);
+    elevat_motor_right.set(-5);
+  }else if(controller1.getAButtonReleased()){
+    elevat_motor_left.set(0);
+    elevat_motor_right.set(0);
+   
+  }
 
-
+   if(controller1.getLeftBumperButtonPressed()){
+    claw_motor_left.set(5);
+    claw_motor_right.set(5);
+  }else if(controller1.getLeftBumperButtonReleased()){
+    claw_motor_left.set(0);
+    claw_motor_right.set(0);
+   }
+  
+   if(controller1.getRightBumperButtonPressed()){
+    claw_motor_left.set(-5);
+    claw_motor_right.set(-5);
+  }else if(controller1.getRightBumperButtonReleased()){
+    claw_motor_left.set(0);
+    claw_motor_right.set(0);
+   
+  }
   }
 
   @Override
